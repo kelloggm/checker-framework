c3b2d4b7d079bd075b9c09fa295d5bed019af321
Martin Kellogg <kelloggm@cs.washington.edu>
2020-02-13 15:43:48 -0800

    don't print bad stub file wrt wildcards in inferred return types

--- framework/src/main/java/org/checkerframework/common/wholeprograminference/SceneToStubWriter.java
+++ framework/src/main/java/org/checkerframework/common/wholeprograminference/SceneToStubWriter.java
@@ -471,9 +471,16 @@ public final class SceneToStubWriter {
             // method. A better type is printed if one is available to improve
             // the readability of the resulting stub file.
             String descriptionString = aMethod.returnType.description.toString();
+            boolean availableReturnType = false;
             if (basetypes.containsKey(descriptionString)) {
-                printWriter.print(basetypes.get(descriptionString));
-            } else {
+                String returnTypeString = basetypes.get(descriptionString).toString();
+                // do not print return types that start with a ?, because that's not valid Java
+                if (!returnTypeString.startsWith("?")) {
+                    availableReturnType = true;
+                    printWriter.print(returnTypeString);
+                }
+            }
+            if (!availableReturnType) {
                 printWriter.print("java.lang.Object");
             }
             printWriter.print(" ");
--- framework/tests/whole-program-inference/non-annotated/WildcardReturn.java
+++ framework/tests/whole-program-inference/non-annotated/WildcardReturn.java
@@ -1,15 +1,11 @@
-// This test ensures that generated stub files handle methods that have a
-// wildcard return type correctly.
+// This test ensures that generated stub files handle methods that have an
+// inferred wildcard return type correctly.
 
 import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 class WildcardReturn {
-    public static <T extends Comparable<? super T>> T max(Collection<T> coll) {
-        return null;
-    }
-
     public Set<Object> getCredentialIdsForUsername(String username) {
         return getRegistrationsByUsername(username).stream()
                 .map(registration -> registration.toString())
